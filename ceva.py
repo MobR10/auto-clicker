26729713a79e645114875f2ff97a6ab0760ed3d2
MobR10 <100953132+MobR10@users.noreply.github.com>
2023-09-18 22:49:56 +0300

    18.09.2023 22:49 update

--- ceva.py
+++ ceva.py
@@ -6,24 +6,21 @@ from tkinter import *
 from Keys import Keys
 from SaveConfig import SaveConfig
 
-
 def repeat_function():
-    if repeat_enable.state:
-        print('Repeat Enabled')
-        while repeat_enable.state and not changing_state and repeat_button.value!='None':
+    if repeat_enable.state and repeat_button.value!='None':
+        print('Repeat Enabled\n')
+        while repeat_enable.state and not changing_state:
                 kb.press_and_release(repeat_button.value)
                 time.sleep(0.03)
-        repeat_enable.state=False
         print("Repeat Disabled\n")
     
 def press_function():
-    if press_enable.state and not changing_state and press_button.value!='None':
+    if press_enable.state and press_button.value!='None':
         kb.press(press_button.value)
-        print('Press Enabled')
-    else:
+        print('Press Enabled\n')
+    elif press_button.value!='None':
         kb.release(press_button.value)
-        press_enable.state=False
-        print('Stopped Pressing')    
+        print('Stopped Pressing\n')    
 
 # CHANGING BUTTONS
 changing_state=False
@@ -39,13 +36,18 @@ press_button=Keys(name='press_button',description='Choose button to press once a
 # ARRAY
 enable_keys=[repeat_enable,press_enable]
 
+def are_same_keys():
+    if repeat_enable.value == press_enable.value and repeat_enable.value!='None':
+        return True
+    else:
+        return False
+
 # SAVE FILE
 save=SaveConfig(repeat_enable,repeat_button,press_enable,press_button)
 
-
 def  main(e):
     if not changing_state:
-        if(e.event_type=='down'):
+        if e.event_type=='down':
             for key in enable_keys:
                 if e.name==key.value and not key.pressed:
                     key.pressed=True
@@ -60,12 +62,17 @@ def  main(e):
 def main_change(e):
     global changing_state,changing_button
     if changing_state:
+        abort=False
         if(e.event_type=='down'):
-            changing_button.value=e.name
-            print('Changed value for '+changing_button.name+' in '+e.name)
+            for key in enable_keys:
+                if changing_button!=key and e.name==key.value and changing_button!=repeat_button and changing_button!= press_button:
+                    print("You tried to attribute the same value to "+ changing_button.name+ " as "+ key.name+"'s value! REFUSED")
+                    abort=True
+            if not abort:
+                changing_button.value=e.name
+                print('Changed value for '+changing_button.name+' in '+e.name)
         changing_state=False
-            
-  
+        
 def change_button(button):
     global changing_state,changing_button
     if changing_state==False:
@@ -88,28 +95,32 @@ if __name__=='__main__':
     window.protocol("WM_DELETE_WINDOW", on_closing) 
      
     #  CONFIGURATION
+    
     save.read_config()
+    if are_same_keys():
+        print("Values for some keys who should not have the same value are the same. Changing to None")
+        repeat_enable.value='None'
+        press_enable.value='None'
+
     # INSTRUCTIONS
 
-if repeat_enable.value!='None' and repeat_button.value!='None':
-    print(f"Press {repeat_enable.value.capitalize()} to keep pressing and releasing {repeat_button.value.capitalize()}!")  
-if press_enable.value!='None' and press_button.value!='None':    
-    print(f"Press {press_enable.value.capitalize()} to keep pressing {press_button.value.capitalize()}!\n")
+    if repeat_enable.value!='None' and repeat_button.value!='None':
+        print(f"Press {repeat_enable.value.capitalize()} to keep pressing and releasing {repeat_button.value.capitalize()}!")  
+    if press_enable.value!='None' and press_button.value!='None':    
+        print(f"Press {press_enable.value.capitalize()} to keep pressing {press_button.value.capitalize()}!\n")
 
     kb.hook(main)
     kb.hook(main_change)
-    
+        
     window_repeat_enable=Button(window,text="Repeat Enable",fg='green',bg='black',font=('Arial',10,"bold"),command=lambda: change_button(repeat_enable))
     window_repeat_button=Button(window,text="Repeat Button",fg='green',bg='black',font=('Arial',10,"bold"),command=lambda: change_button(repeat_button))
-    
+        
     window_press_enable=Button(window,text="Press Enable",fg='green',bg='black',font=('Arial',10,"bold"),command=lambda: change_button(press_enable))
     window_press_button=Button(window,text="Press Button",fg='green',bg='black',font=('Arial',10,"bold"),command=lambda: change_button(press_button))
-    
+        
     window_repeat_enable.place(x=0,y=0)
     window_repeat_button.place(x=110,y=0)
-    
+        
     window_press_enable.place(x=0,y=100)
     window_press_button.place(x=100,y=100)
     window.mainloop()
-
-
